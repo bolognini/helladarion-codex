eebd5c7c83fe2c0f592059b79b6bc39539a4706f
bolognini <matheusbolognini79@gmail.com>
2021-05-24 14:19:11 -0300

    feat: update site metadata

--- gatsby/wrapRootElement.js
+++ gatsby/wrapRootElement.js
@@ -1,13 +1,18 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 import { ThemeProvider } from 'styled-components'
 import { Reset } from 'styled-reset'
 import { GlobalStyle } from '../src/styles/GlobalStyle'
 import light from '../src/styles/themes/light'
 
-
 export function wrapRootElement({ element }) {
   return (
     <>
+      <Helmet>
+        <meta charSet='utf-8' />
+        <title>Helladarion Codex</title>
+        <link rel='canonical' href='https://helladarion-codex.netlify.app/' />
+      </Helmet>
       <Reset />
       <GlobalStyle />
       <ThemeProvider theme={light}>{element}</ThemeProvider>
